f471ae419bdfe7f9c0cd8526d2189c257fdbd71c
Shray0611 <164644420+Shray0611@users.noreply.github.com>
2025-03-01 12:57:24 +0530

    temp

--- src/UserHome.jsx
+++ src/UserHome.jsx
@@ -37,7 +37,7 @@ const UserHome = ({ onLogout }) => {
 
   const handleComplaint = (certificate) => {
     console.log("Raising complaint for:", certificate);
-    navigate("/raise-complaint");
+    navigate("/report-issue/id");
   };
 
   return (
